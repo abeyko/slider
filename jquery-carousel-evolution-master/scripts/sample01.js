e334d3b74200387bf965f2433556ef43c5ae7cae
Angeliki Beyko <abeyko@msn.com>
2016-07-17 20:01:55 -0500

    first commit

---
+++ jquery-carousel-evolution-master/scripts/sample01.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function(){
+	$('.carousel').carousel({
+		carouselWidth:930,
+		carouselHeight:330,
+		directionNav:true,
+		shadow:true,
+		buttonNav:'bullets'
+	});
+	$(function() {
+    
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+	});
+	/*
+	if (bullet.eq(currentSlide)) {
+		$("h2").show();
+	} else {
+		$("h2").hide();
+	}
+	*/
+});
